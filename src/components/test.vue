8716410ad5a8021b37480d65b9791be772704bc5
varwx <varwx163@163.com>
2017-11-11 01:30:55 +0800

    新增登录

---
+++ src/components/test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="login">
+    <div class="wrapper">
+      <div class="login">
+        <form action="#" method="post" class="container offset1 loginform">
+          <div id="owl-login" class="">
+            <div class="hand"></div>
+            <div class="hand hand-r"></div>
+            <div class="arms">
+              <div class="arm"></div>
+              <div class="arm arm-r"></div>
+            </div>
+          </div>
+          <div class="pad">
+            <input type="hidden" name="_csrf" value="9IAtUxV2CatyxHiK2LxzOsT6wtBE6h8BpzOmk=">
+            <div class="control-group">
+              <div class="controls">
+                <label for="username" class="glyphicon glyphicon-user"></label>
+                <input id="username" type="text" name="username" placeholder="Email" tabindex="1" autofocus="autofocus" class="form-control input-medium">
+              </div>
+            </div>
+            <div class="control-group">
+              <div class="controls">
+                <label for="password" class="glyphicon glyphicon-lock"></label>
+                <input id="password" type="password" name="password" placeholder="Password" tabindex="2" class="form-control input-medium">
+              </div>
+            </div>
+          </div>
+          <div class="form-actions">
+            <a href="#" tabindex="5" class="btn pull-left btn-link text-muted">Forgot password?</a>
+            <a href="#" tabindex="6" class="btn btn-link text-muted" id="register-in">Sign Up</a>
+            <button type="button" tabindex="4" class="btn btn-primary" id="login-in">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    @import '../assets/css/bootstrap.min.css';
+    @import '../assets/css/login.css';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import test from '@/components/test'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'Hello',
+      component: test
     }
   ]
 })
